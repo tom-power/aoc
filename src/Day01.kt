78934fa33cf5c814a834acf4c2c7da2964adda7c
Tom Power <tp21@tuta.io>
2022-12-01 22:28:59 +0000

    tidy

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,7 @@
 object Day01 : Day {
-    override fun List<String>.part1(): Int = partitioned().max()
+    override fun List<String>.part1(): Int = partitionedBy("").summed().max()
 
-    override fun List<String>.part2(): Int = partitioned().sumTop3()
+    override fun List<String>.part2(): Int = partitionedBy("").summed().sumOfTop3()
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -9,12 +9,14 @@ object Day01 : Day {
     }
 }
 
-fun List<String>.partitioned(): List<Int> =
-    partitionAt(this.indexesOf(""))
-        .map { it.map(String::toInt) }
+fun List<String>.partitionedBy(delimiter: String): List<List<String>> =
+    partitionAt(this.indexesOf(delimiter))
+
+fun List<List<String>>.summed(): List<Int> =
+    this.map { it.map(String::toInt) }
         .map(List<Int>::sum)
 
-fun List<Int>.sumTop3(): Int =
+fun List<Int>.sumOfTop3(): Int =
     sortedByDescending { it }
         .take(3)
         .sum()
