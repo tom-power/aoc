4421607eaeb5948649a0b7d3a7f02f158296b731
Tom Power <tpower21@gmail.com>
2022-12-01 22:07:33 +0000

    abstract an interface

---
+++ src/Day00.kt
@@ -0,0 +1,10 @@
+object Day00 : Day {
+    override fun List<String>.part1(): Int = TODO()
+
+    override fun List<String>.part2(): Int = TODO()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        super.runMeWith(partOneTestResult = TODO())
+    }
+}
