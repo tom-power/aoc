c63db29796af28e66cec367995a700b76ea2c097
Tom Power <tpower21@gmail.com>
2022-12-02 07:48:41 +0000

    add init day

--- src/test/kotlin/Day00Test.kt
+++ src/test/kotlin/Day00Test.kt
@@ -1,12 +1,17 @@
+import Day00.part1
+import Day00.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day00Test {
 
     @Test
+    @Disabled
     fun `is ok`() {
-//        assertEquals(Day00.readDaysInputTest().part1(), 0)
-//        assertEquals(Day00.readDaysInputTest().part2(), 0)
-//        assertEquals(Day00.readDaysInput().part1(), 0)
-//        assertEquals(Day00.readDaysInput().part2(), 0)
+        assertEquals(Day00.readDaysInputTest().part1(), 0)
+        assertEquals(Day00.readDaysInputTest().part2(), 0)
+        assertEquals(Day00.readDaysInput().part1(), 0)
+        assertEquals(Day00.readDaysInput().part2(), 0)
     }
 }
