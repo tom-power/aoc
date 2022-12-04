69edd3c13a1c72040a9978925daddc48703b2b05
Tom Power <tpower21@gmail.com>
2022-12-04 16:13:19 +0000

    namespace

--- src/main/kotlin/aoc22/Day04.kt
+++ src/main/kotlin/aoc22/Day04.kt
@@ -4,26 +4,26 @@ object Day04 : Day {
     override fun List<String>.part1(): Int = countOfAssignmentPairsWhere(::isOverlapAll)
 
     override fun List<String>.part2(): Int = countOfAssignmentPairsWhere(::isOverlapAny)
+
+    private fun List<String>.countOfAssignmentPairsWhere(predicate: (Pair<IntRange, IntRange>) -> Boolean): Int =
+        map { it.toAssignmentPairs() }
+            .filter { predicate(it) }
+            .count()
+
+    private fun String.toAssignmentPairs(): Pair<IntRange, IntRange> =
+        split(",")
+            .run { this[0].toRange() to this[1].toRange() }
+
+    private fun String.toRange(): IntRange =
+        split("-")
+            .run { this[0].toInt()..this[1].toInt() }
+
+    private fun isOverlapAll(ranges: Pair<IntRange, IntRange>) =
+        ranges.first.intersect(ranges.second).let { it == ranges.first.toSet() || it == ranges.second.toSet() }
+
+    private fun isOverlapAny(ranges: Pair<IntRange, IntRange>) =
+        ranges.first.intersect(ranges.second).isNotEmpty()
+
+    private fun IntRange.intersect(other: IntRange) =
+        this.toSet().intersect(other.toSet())
 }
-
-fun List<String>.countOfAssignmentPairsWhere(predicate: (Pair<IntRange, IntRange>) -> Boolean): Int =
-    map { it.toAssignmentPairs() }
-        .filter { predicate(it) }
-        .count()
-
-fun String.toAssignmentPairs(): Pair<IntRange, IntRange> =
-    split(",")
-        .run { this[0].toRange() to this[1].toRange() }
-
-fun String.toRange(): IntRange =
-    split("-")
-        .run { this[0].toInt()..this[1].toInt() }
-
-fun isOverlapAll(ranges: Pair<IntRange, IntRange>) =
-    ranges.first.intersect(ranges.second).let { it == ranges.first.toSet() || it == ranges.second.toSet() }
-
-fun isOverlapAny(ranges: Pair<IntRange, IntRange>) =
-    ranges.first.intersect(ranges.second).isNotEmpty()
-
-fun IntRange.intersect(other: IntRange) =
-    this.toSet().intersect(other.toSet())
