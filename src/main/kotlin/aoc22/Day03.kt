dc09fca4f198f6987f15656c2d227b73b7db9c24
Tom Power <tpower21@gmail.com>
2022-12-04 15:20:47 +0000

    tidy day 3

--- src/main/kotlin/aoc22/Day03.kt
+++ src/main/kotlin/aoc22/Day03.kt
@@ -1,19 +1,21 @@
 package aoc22
 
 object Day03 : Day {
-    override fun List<String>.part1(): Int = prioritiesCompartments()
+    override fun List<String>.part1(): Int = toCompartments().toSumOfCommon()
 
-    override fun List<String>.part2(): Int = prioritiesBadges()
+    override fun List<String>.part2(): Int = toTrios().toSumOfCommon()
 }
 
-fun List<String>.prioritiesCompartments(): Int =
-    map { it.chunked(it.count() / 2) }
-        .map { it.commonItem() }
-        .sumOf { it.toPriority() }
+fun List<String>.toCompartments(): List<List<String>> =
+    map { rucksackItems ->
+        rucksackItems.chunked(rucksackItems.count() / 2)
+    }
 
-fun List<String>.prioritiesBadges(): Int =
+fun List<String>.toTrios(): List<List<String>> =
     chunked(3)
-        .map { it.commonItem() }
+
+fun List<List<String>>.toSumOfCommon(): Int =
+    map { it.commonItem() }
         .sumOf { it.toPriority() }
 
 fun List<String>.commonItem(): Char =
@@ -21,4 +23,7 @@ fun List<String>.commonItem(): Char =
         s.toSet().intersect(acc.toSet())
     }.first()
 
-fun Char.toPriority(): Int = code - (if (isUpperCase()) (64 - 26) else 96)
+fun Char.toPriority(): Int = code - asciiIndex() + upperCasePriority()
+
+fun Char.asciiIndex(): Int = if (isUpperCase()) 64 else 96
+fun Char.upperCasePriority(): Int = if (isUpperCase()) 26 else 0
