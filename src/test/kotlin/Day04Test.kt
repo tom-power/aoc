0b7ff2e75a599952ae9b0247e0c66997eecfd468
Tom Power <tpower21@gmail.com>
2022-12-04 14:23:45 +0000

    day 4

---
+++ src/test/kotlin/Day04Test.kt
@@ -0,0 +1,28 @@
+import Day04.part1
+import Day04.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class Day04Test {
+
+    @Test
+    fun `part one test`() {
+        assertEquals(2, Day04.readDaysInputTest().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(431, Day04.readDaysInput().part1())
+    }
+
+    @Test
+    fun `part two test`() {
+        assertEquals(4, Day04.readDaysInputTest().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(823, Day04.readDaysInput().part2())
+    }
+}
