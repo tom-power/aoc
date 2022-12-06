9a83a3c4582fcbb7f97587d7cb6a05b800f75df0
Tom Power <tp21@tuta.io>
2022-12-06 05:41:05 +0000

    day 6

---
+++ src/test/kotlin/aoc22/Day06Test.kt
@@ -0,0 +1,29 @@
+package aoc22
+
+import aoc22.Day06.part1
+import aoc22.Day06.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals(7, Day06.readDaysInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(1080, Day06.readDaysInput().part1())
+    }
+
+    @Test
+    fun `part two example`() {
+        assertEquals(19, Day06.readDaysInputExample().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(3645, Day06.readDaysInput().part2())
+    }
+}
