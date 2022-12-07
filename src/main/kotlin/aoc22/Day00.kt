5ecb03e63069e334a37e184eb057fc26c44f5bdb
Tom Power <tp21@tuta.io>
2022-12-07 09:30:43 +0000

    infra

--- src/main/kotlin/aoc22/Day00.kt
+++ src/main/kotlin/aoc22/Day00.kt
@@ -1,17 +1,21 @@
 package aoc22
 
+import aoc22.Day00Solution.part1Day00
+import aoc22.Day00Solution.part2Day00
 import aoc22.Misc.log
 
 object Day00 : Day<String, Int> {
     override fun List<String>.part1(): Int = part1Day00()
 
     override fun List<String>.part2(): Int = part2Day00()
+}
 
-    private fun List<String>.part1Day00(): Int =
+object Day00Solution {
+    fun List<String>.part1Day00(): Int =
         map { it.split(",") }.log()
             .let { 0 }
 
-    private fun List<String>.part2Day00(): Int =
+    fun List<String>.part2Day00(): Int =
         map { it.split(",") }.log()
             .let { 0 }
 }
