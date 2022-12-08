b8feba3947562c21ac46ede1e393eb3ec75ed826
Tom Power <tpower21@gmail.com>
2022-12-08 08:28:00 +0000

    day 8

---
+++ src/test/kotlin/aoc22/Day08Test.kt
@@ -0,0 +1,29 @@
+package aoc22
+
+import aoc22.Day08.part1
+import aoc22.Day08.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day08Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals(21, Day08.readInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(1669, Day08.readInput().part1())
+    }
+
+    @Test
+    fun `part two example`() {
+        assertEquals(8, Day08.readInputExample().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(331344, Day08.readInput().part2())
+    }
+}
