7027ee8a22bb6cfdf6da78737113b2f8badae297
Tom Power <tpower21@gmail.com>
2022-12-10 09:04:14 +0000

    day 10 part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(kotlin("test"))
 }
 
 tasks {
