f9f84275966738bca02f9ab013ac502821836fa0
Tom Power <tp21@tuta.io>
2022-12-11 19:33:02 +0000

    tidy

--- src/main/kotlin/aoc22/Day02.kt
+++ src/main/kotlin/aoc22/Day02.kt
@@ -1,23 +1,64 @@
 package aoc22
 
-import aoc22.Common.summed
-import aoc22.Common.theirPlayMap
-import aoc22.Common.toRoundsUsing
-import aoc22.RockPaperScissorsDomain.Outcome
-import aoc22.RockPaperScissorsDomain.Outcome.*
-import aoc22.RockPaperScissorsDomain.Play
-import aoc22.RockPaperScissorsDomain.Play.*
-import aoc22.RockPaperScissorsDomain.Round
-import aoc22.RockPaperScissorsDomain.points
-import aoc22.RockPaperScissorsDomain.toOutcome
+import aoc22.Day02Common.summed
+import aoc22.Day02Common.toRoundsUsing
+import aoc22.DomainRPS.Outcome
+import aoc22.DomainRPS.Outcome.*
+import aoc22.DomainRPS.Play
+import aoc22.DomainRPS.Play.*
+import aoc22.DomainRPS.Round
+import aoc22.DomainRPS.points
+import aoc22.DomainRPS.toOutcome
 
 object Day02 : Day<String, Int, Int> {
-    override fun List<String>.part1(): Int = toRoundsUsing(Part1::toPlays).summed()
+    override fun List<String>.part1(): Int = toRoundsUsing(Day02Part1::toPlays).summed()
 
-    override fun List<String>.part2(): Int = toRoundsUsing(Part2::toPlaysForOutcome).summed()
+    override fun List<String>.part2(): Int = toRoundsUsing(Day02Part2::toPlaysForOutcome).summed()
 }
 
-object RockPaperScissorsDomain {
+object Day02Common {
+    fun List<String>.toRoundsUsing(toPlays: Keys.() -> Plays): Rounds =
+        with(Day02Parser) { toKeys() }.toPlays().map { it.toRound() }
+
+    private fun Pair<Play, Play>.toRound() = Round(first, second)
+
+    fun List<Round>.summed() = map { it.toOutcome().points() + it.yourPlay.points() }.sum()
+}
+
+private val theirPlayMap = mapOf("A" to Rock, "B" to Paper, "C" to Scissors)
+
+object Day02Part1 {
+    private val yourPlayMap = mapOf("X" to Rock, "Y" to Paper, "Z" to Scissors)
+
+    fun toPlays(keys: Keys): Plays =
+        keys.map { (theirKey, yourKey) ->
+            theirPlayMap[theirKey]!! to yourPlayMap[yourKey]!!
+        }
+}
+
+object Day02Part2 {
+    private val yourOutcomeMap = mapOf("X" to Lose, "Y" to Draw, "Z" to Win)
+
+    fun toPlaysForOutcome(keys: Keys): Plays =
+        keys.map { (theirKey, yourKey) ->
+            theirPlayMap[theirKey]!!.let { theirPlay ->
+                theirPlay to theirPlay.playFor(yourOutcomeMap[yourKey]!!)
+            }
+        }
+
+    private fun Play.playFor(outcome: Outcome): Play =
+        Play.values().first { Round(this, it).toOutcome() == outcome }
+}
+
+object Day02Parser {
+    fun List<String>.toKeys(): Keys = map { it.split(" ").run { this[0] to this[1] } }
+}
+
+typealias Keys = List<Pair<String, String>>
+typealias Plays = List<Pair<Play, Play>>
+typealias Rounds = List<Round>
+
+object DomainRPS {
     enum class Play {
         Rock, Paper, Scissors
     }
@@ -53,43 +94,4 @@ object RockPaperScissorsDomain {
             Draw -> 3
             Lose -> 0
         }
-}
-
-object Common {
-    val theirPlayMap = mapOf("A" to Rock, "B" to Paper, "C" to Scissors)
-
-    private fun List<String>.toKeys(): Keys = map { it.split(" ").run { this[0] to this[1] } }
-
-    private fun Pair<Play, Play>.toRound() = Round(first, second)
-
-    fun List<String>.toRoundsUsing(toPlays: Keys.() -> Plays): Rounds = toKeys().toPlays().map { it.toRound() }
-
-    fun List<Round>.summed() = map { it.toOutcome().points() + it.yourPlay.points() }.sum()
-}
-
-typealias Keys = List<Pair<String, String>>
-typealias Plays = List<Pair<Play, Play>>
-typealias Rounds = List<Round>
-
-object Part1 {
-    private val yourPlayMap = mapOf("X" to Rock, "Y" to Paper, "Z" to Scissors)
-
-    fun toPlays(keys: Keys): Plays =
-        keys.map { (theirKey, yourKey) ->
-            theirPlayMap[theirKey]!! to yourPlayMap[yourKey]!!
-        }
-}
-
-object Part2 {
-    private val yourOutcomeMap = mapOf("X" to Lose, "Y" to Draw, "Z" to Win)
-
-    fun toPlaysForOutcome(keys: Keys): Plays =
-        keys.map { (theirKey, yourKey) ->
-            theirPlayMap[theirKey]!!.let { theirPlay ->
-                theirPlay to theirPlay.playFor(yourOutcomeMap[yourKey]!!)
-            }
-        }
-
-    private fun Play.playFor(outcome: Outcome): Play =
-        Play.values().first { Round(this, it).toOutcome() == outcome }
 }
--- src/main/kotlin/aoc22/Day07.kt
+++ src/main/kotlin/aoc22/Day07.kt
@@ -1,57 +1,157 @@
 package aoc22
 
+import aoc22.Day07Domain.Command
+import aoc22.Day07Domain.Down
+import aoc22.Day07Domain.Ls
+import aoc22.Day07Domain.Cd
+import aoc22.Day07Domain.Up
+import aoc22.Day07Solution.part1Day07
+import aoc22.Day07Solution.part2Day07
+
 object Day07 : Day<String, Int, Int> {
-    override fun List<String>.part1(): Int = with(FileSys()) { dirSizes().sumOfSmall() }
+    override fun List<String>.part1(): Int = part1Day07()
 
-    override fun List<String>.part2(): Int = with(FileSys()) { dirSizes().findSmallestToDelete() }
+    override fun List<String>.part2(): Int = part2Day07()
 }
 
-private fun Map<String, Int>.sumOfSmall(): Int =
-    values
-        .filter { it < 100_000 }
-        .sum()
+object Day07Solution {
+    fun List<String>.part1Day07(): Int = getDirSizes().sumOfSmall()
 
-private val totalSpace = 70_000_000
-private val neededSpace = 30_000_000
+    private fun List<Int>.sumOfSmall(): Int =
+        filter { it < 100_000 }.sum()
 
-private fun Map<String, Int>.findSmallestToDelete(): Int {
-    val usedSpace = values.max()
-    return values
-        .filter { (neededSpace + usedSpace - it) < totalSpace }
-        .min()
+    fun List<String>.part2Day07(): Int = getDirSizes().findSmallestToDelete()
+
+    private val totalSpace = 70_000_000
+    private val neededSpace = 30_000_000
+
+    private fun List<Int>.findSmallestToDelete(): Int =
+        filter { (neededSpace + max() - it) < totalSpace }.min()
+
+    private fun List<String>.getDirSizes(): List<Int> =
+        with(Day07Parser) {
+            with(Day07FileSys()) {
+                parseCommands().dirSizes()
+            }
+        }
 }
 
-class FileSys {
+class Day07FileSys {
     private var pwd = "/"
     private val parents = mutableListOf<String>()
     private val dirSizeMap = mutableMapOf("/" to 0)
 
-    fun List<String>.dirSizes(): MutableMap<String, Int> =
+    fun List<Command>.dirSizes(): List<Int> =
         updateDirSizeMap()
-            .let { dirSizeMap }
+            .let { dirSizeMap.values.toList() }
 
-    private fun List<String>.updateDirSizeMap() {
-        forEach { row ->
-            when {
-                row.startsWith("\$ cd ") -> row.replace("\$ cd ", "").updateLocation()
-                row.first().isDigit()    -> (row.filter { it.isDigit() }.toIntOrNull() ?: 0).updateDirSizeMap()
+    private fun List<Command>.updateDirSizeMap() {
+        forEach {
+            when (it) {
+                is Cd -> it.updateLocation()
+                is Ls -> it.updateDirSizeMap()
             }
         }
     }
 
-    private fun getDirKey(indexTo: Int, pwd: String): String = parents.subList(0, indexTo).joinToString("/") + pwd
-
-    private fun Int.updateDirSizeMap() {
-        dirSizeMap[getDirKey(parents.size, pwd)] = this + (dirSizeMap[getDirKey(parents.size, pwd)] ?: 0)
+    private fun Ls.updateDirSizeMap() {
+        dirSizeMap[getDirKey(parents.size, pwd)] = this.fileSize + (dirSizeMap[getDirKey(parents.size, pwd)] ?: 0)
         parents.forEachIndexed { index, parent ->
-            dirSizeMap[getDirKey(index, parent)] = this + (dirSizeMap[getDirKey(index, parent)] ?: 0)
+            dirSizeMap[getDirKey(index, parent)] = this.fileSize + (dirSizeMap[getDirKey(index, parent)] ?: 0)
         }
     }
 
-    private fun String.updateLocation() {
+    private fun Cd.updateLocation() {
         when (this) {
-            ".." -> parents.removeLast()
-            else -> (this).also { parents.add(pwd) }
+            Up      -> parents.removeLast()
+            is Down -> this.path.also { parents.add(pwd) }
         }.let { pwd = it }
     }
+
+    private fun getDirKey(indexTo: Int, pwd: String): String = parents.subList(0, indexTo).joinToString("/") + pwd
+
+}
+
+class Day07FileSysFold {
+    data class State(
+        var pwd: String,
+        val parents: List<String>,
+        val dirSizeMap: Map<String, Int>,
+    )
+
+    fun List<Command>.dirSizes(): List<Int> {
+        val initialState =
+            State(
+                pwd = "/",
+                parents = emptyList(),
+                dirSizeMap = mutableMapOf("/" to 0)
+            )
+
+        return fold(initialState) { acc, command ->
+            when (command) {
+                is Cd -> acc.cd(command)
+                is Ls -> acc.ls(command)
+            }
+        }.dirSizeMap.values.toList()
+    }
+
+    private fun State.cd(cd: Cd): State =
+        when (cd) {
+            is Down -> this.cdDown(cd)
+            Up      -> this.cdUp(cd as Up)
+        }
+
+    private fun State.cdDown(down: Down): State =
+        copy(
+            pwd = down.path,
+            parents = parents + listOf(down.path)
+        )
+
+    private fun State.cdUp(up: Up): State =
+        copy(
+            pwd = parents.last(),
+            parents = parents.dropLast(1)
+        )
+
+    private fun State.ls(ls: Ls): State {
+        val dirKey = getDirKey(parents.size, pwd)
+        val thisDirSizeMap = mutableMapOf<String, Int>()
+
+        thisDirSizeMap[dirKey] = ls.fileSize + (dirSizeMap[dirKey] ?: 0)
+        parents.forEachIndexed { index, parent ->
+            val dirKey1 = getDirKey(index, parent)
+            thisDirSizeMap[dirKey1] = ls.fileSize + (dirSizeMap[dirKey1] ?: 0)
+        }
+
+        return copy(dirSizeMap = dirSizeMap + thisDirSizeMap)
+    }
+
+    private fun State.getDirKey(indexTo: Int, pwd: String): String = parents.subList(0, indexTo).joinToString("/") + pwd
+
+}
+
+object Day07Domain {
+    sealed class Command
+    sealed class Cd : Command()
+    object Up : Cd()
+    data class Down(val path: String) : Cd()
+    data class Ls(val fileSize: Int) : Command()
+}
+
+object Day07Parser {
+    fun List<String>.parseCommands(): List<Command> =
+        mapNotNull {
+            when {
+                it.startsWith("\$ cd ") ->
+                    it.replace("\$ cd ", "").let { cd ->
+                        when (cd) {
+                            ".." -> Up
+                            else -> Down(cd)
+                        }
+                    }
+
+                it.first().isDigit()    -> Ls((it.filter { it.isDigit() }.toIntOrNull() ?: 0))
+                else                    -> null
+            }
+        }
 }
