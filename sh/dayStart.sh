ab63f6ba3d10edad6590816b8b3ed7d3bca48433
Tom Power <tp21@tuta.io>
2022-12-16 20:18:48 +0000

    drop dayEnd

--- sh/dayStart.sh
+++ sh/dayStart.sh
@@ -2,10 +2,10 @@
 
 source $PWD/sh/paths.sh
 
-cp "$srcMainAoc/Day00.kt" "$srcMain/Day$1.kt" &&
+cp "$srcMain/Day00.kt" "$srcMain/Day$1.kt" &&
 sed -i "s/00/$1/g" "$srcMain/Day$1.kt" &&
 
-cp "$srcTestAoc/Day00Test.kt" "$srcTest/Day$1Test.kt" &&
+cp "$srcTest/Day00Test.kt" "$srcTest/Day$1Test.kt" &&
 sed -i "s/00/$1/g" "$srcTest/Day$1Test.kt" &&
 
 touch "$srcResources/Day$1.txt"  &&
