47d2539a4c776bf350d8c95eeeaac0b96e38dc7a
Tom Power <tpower21@gmail.com>
2023-01-06 15:12:23 +0000

    Day21

---
+++ src/test/kotlin/aoc22/Day21Test.kt
@@ -0,0 +1,29 @@
+package aoc22
+
+import aoc22.Day21.part1
+import aoc22.Day21.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day21Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals(152, Day21.readInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(49288254556480, Day21.readInput().part1())
+    }
+
+    @Test
+    fun `part two example`() {
+        assertEquals(301, Day21.readInputExample().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(3558714869436, Day21.readInput().part2())
+    }
+}
