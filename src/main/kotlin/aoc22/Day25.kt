6a4e463a91e735d383358a960a9b591c987437fc
Tom Power <tpower21@gmail.com>
2023-05-24 10:03:01 +0100

    Day 25

---
+++ src/main/kotlin/aoc22/Day25.kt
@@ -0,0 +1,65 @@
+package aoc22
+
+import aoc22.Day25Domain.BaseConvertor.toBalancedQuinary
+import aoc22.Day25Domain.BaseConvertor.toDecimal
+import aoc22.Day25Domain.SnafuConvertor.toBalancedQuinary
+import aoc22.Day25Domain.SnafuConvertor.toSnafu
+import aoc22.Day25Solution.part1Day25
+import kotlin.math.pow
+
+object Day25 : Day {
+    fun List<Snafu>.part1(): Snafu = part1Day25()
+}
+
+object Day25Solution {
+    fun List<Snafu>.part1Day25(): Snafu =
+        map { snafu -> snafu.toBalancedQuinary().toDecimal() }.sum()
+            .toBalancedQuinary()
+            .toSnafu()
+}
+
+private typealias Decimal = Long
+private typealias BalancedQuinary = List<Int>
+private typealias Snafu = String
+
+object Day25Domain {
+    object BaseConvertor {
+        fun BalancedQuinary.toDecimal(): Decimal =
+            reversed().foldIndexed(0L) { index, acc, i ->
+                acc +
+                    when (index) {
+                        0 -> i
+                        else -> 5.toDouble().pow(index) * i
+                    }.toLong()
+            }
+
+        fun Decimal.toBalancedQuinary(): BalancedQuinary =
+            generateSequence(this) { (it + 2) / 5 }.takeWhile { it != 0L }
+                .map {
+                    when(val mod = (it % 5).toInt()) {
+                        3 -> -2
+                        4 -> -1
+                        else -> mod
+                    }
+                }
+                .toList()
+                .reversed()
+    }
+
+    object SnafuConvertor {
+        private val snafuIntMap =
+            mapOf(
+                '=' to -2,
+                '-' to -1,
+                '0' to 0,
+                '1' to 1,
+                '2' to 2,
+            )
+
+        fun Snafu.toBalancedQuinary(): BalancedQuinary = map { snafuIntMap[it]!! }
+
+        fun BalancedQuinary.toSnafu(): Snafu =
+            map { int -> snafuIntMap.filter { it.value == int }.keys.single() }
+                .joinToString("")
+    }
+}
---
+++ src/test/kotlin/aoc22/Day25Test.kt
@@ -0,0 +1,18 @@
+package aoc22
+
+import aoc22.Day25.part1
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day25Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals("2=-1=0", Day25.readInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals("20=02=120-=-2110-0=1", Day25.readInput().part1())
+    }
+}
