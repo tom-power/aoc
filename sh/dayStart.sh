fd9b8c78e5e3d1ad6a61b15f60f242b933f6d5e5
Tom Power <tpower21@gmail.com>
2023-12-02 14:13:25 +0000

    make template input files in test resources

--- sh/dayStart.sh
+++ sh/dayStart.sh
@@ -3,9 +3,9 @@ source $PWD/sh/.env
 day=$1
 mainTemplate="src/main/kotlin/template"
 mainYear="src/main/kotlin/aoc$year"
-mainResourcesYear="src/main/resources/aoc$year"
 testTemplate="src/test/kotlin/template"
 testYear="src/test/kotlin/aoc$year"
+testResourcesYear="src/test/resources/aoc$year"
 
 mkdir -p "$mainYear" &&
 cp "$mainTemplate/Day00.kt" "$mainYear/Day$1.kt" &&
@@ -19,6 +19,6 @@ sed -i "s/Year00/Year$year/g" "$testYear/Day$1Test.kt" &&
 sed -i "s/00/$1/g" "$testYear/Day$1Test.kt" &&
 sed -i "s/template/aoc$year/" "$testYear/Day$1Test.kt"
 
-mkdir -p "$mainResourcesYear"  &&
-touch "$mainResourcesYear/Day$1.txt"  &&
-touch "$mainResourcesYear/Day$1_example.txt"
+mkdir -p "$testResourcesYear"  &&
+touch "$testResourcesYear/Day$1.txt"  &&
+touch "$testResourcesYear/Day$1_example.txt"
