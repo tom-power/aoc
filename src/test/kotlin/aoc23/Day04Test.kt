52db4a512a7db637462399eeb89e0bf2d1fd5b74
Tom Power <tp21@tuta.io>
2023-12-04 07:46:54 +0000

    day 04 23

---
+++ src/test/kotlin/aoc23/Day04Test.kt
@@ -0,0 +1,31 @@
+package aoc23
+
+import common.readInput
+import common.readInputExample
+import aoc23.Day04.part1
+import aoc23.Day04.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day04Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals(13, Day04.readInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(20829, Day04.readInput().part1())
+    }
+
+    @Test
+    fun `part two example`() {
+        assertEquals(30, Day04.readInputExample().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(12648035, Day04.readInput().part2())
+    }
+}
