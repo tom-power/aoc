850a9763f5be3b9c05f093e6ba13a2714517f88d
Tom Power <tp21@tuta.io>
2023-12-04 06:04:33 +0000

    parse domain in template

--- src/main/kotlin/template/Day00.kt
+++ src/main/kotlin/template/Day00.kt
@@ -1,9 +1,11 @@
 package template
 
-import template.Day00Solution.part1Day00
-import template.Day00Solution.part2Day00
 import common.Misc.log
 import common.Year00
+import template.Day00Domain.Day00Type
+import template.Day00Parser.toDay00
+import template.Day00Solution.part1Day00
+import template.Day00Solution.part2Day00
 
 object Day00 : Year00() {
     fun List<String>.part1(): Int = part1Day00()
@@ -13,22 +15,19 @@ object Day00 : Year00() {
 
 object Day00Solution {
     fun List<String>.part1Day00(): Int =
-        map { it.split(",") }.log()
+        toDay00().log()
             .let { 0 }
 
     fun List<String>.part2Day00(): Int =
-        map { it.split(",") }.log()
+        toDay00().log()
             .let { 0 }
 }
 
 object Day00Runner {
 
 }
-
-private typealias Day00Type = Any
-
 object Day00Domain {
-
+    data class Day00Type(val value: String)
 }
 
 object Day00Parser {
