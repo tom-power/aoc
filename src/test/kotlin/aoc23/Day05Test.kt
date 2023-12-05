8e37b11f5cc4fc8338561a25ab1ceb584f37715f
Tom Power <tpower21@gmail.com>
2023-12-05 07:53:07 +0000

    day 05 23

---
+++ src/test/kotlin/aoc23/Day05Test.kt
@@ -0,0 +1,31 @@
+package aoc23
+
+import common.readInput
+import common.readInputExample
+import aoc23.Day05.part1
+import aoc23.Day05.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals(35, Day05.readInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(993500720, Day05.readInput().part1())
+    }
+
+    @Test
+    fun `part two example`() {
+        assertEquals(0, Day05.readInputExample().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(0, Day05.readInput().part2())
+    }
+}
