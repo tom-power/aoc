edf2db68045f64dfec43d65dd66e1d164bebc201
Tom Power <tp21@tuta.io>
2023-12-06 07:17:56 +0000

    day 06 23

---
+++ src/main/kotlin/aoc23/Day06.kt
@@ -0,0 +1,66 @@
+package aoc23
+
+import common.Misc.log
+import common.Year23
+import aoc23.Day06Domain.DesertIsland
+import aoc23.Day06Parser.toDesertIsland
+import aoc23.Day06Solution.part1Day06
+import aoc23.Day06Solution.part2Day06
+import common.Collections.product
+
+object Day06 : Year23() {
+    fun List<String>.part1(): Long = part1Day06()
+
+    fun List<String>.part2(): Long = part2Day06()
+}
+
+object Day06Solution {
+    fun List<String>.part1Day06(): Long =
+        toDesertIsland()
+            .numberOfWaysToBeatRecords()
+
+    fun List<String>.part2Day06(): Long =
+        toDesertIsland()
+            .numberOfWaysToBeatRecord()
+}
+
+object Day06Domain {
+    data class RaceRecord(val time: Long, val distance: Long)
+    data class Race(val buttonTime: Long, val duration: Long)
+    data class DesertIsland(
+        val records: List<RaceRecord>
+    ) {
+        private val record: RaceRecord =
+            RaceRecord(
+                time = records.joinToString("") { it.time.toString() }.toLong(),
+                distance = records.joinToString("") { it.distance.toString() }.toLong()
+            )
+
+        fun numberOfWaysToBeatRecords(): Long = records.map { numberOfWaysToBeat(it) }.product()
+        fun numberOfWaysToBeatRecord(): Long = numberOfWaysToBeat(record)
+
+        private fun numberOfWaysToBeat(record: RaceRecord): Long =
+            (0..record.time).toList()
+                .filter { buttonTime -> distanceFor(buttonTime, record.time) > record.distance }
+                .size
+                .toLong()
+
+        private fun distanceFor(buttonTime: Long, duration: Long): Long = (duration - buttonTime) * buttonTime
+    }
+}
+
+object Day06Parser {
+    fun List<String>.toDesertIsland(): DesertIsland {
+        fun String.toLongs(): List<Long> =
+            this
+                .split(":").last().split(" ")
+                .filterNot { it.isBlank() }.map { it.toLong() }
+
+        val times = this[0].toLongs()
+        val distances = this[1].toLongs()
+        return DesertIsland(
+            records = times.zip(distances).map { Day06Domain.RaceRecord(it.first, it.second.toLong()) }
+        )
+    }
+
+}
---
+++ src/test/kotlin/aoc23/Day06Test.kt
@@ -0,0 +1,31 @@
+package aoc23
+
+import common.readInput
+import common.readInputExample
+import aoc23.Day06.part1
+import aoc23.Day06.part2
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+
+    @Test
+    fun `part one example`() {
+        assertEquals(288, Day06.readInputExample().part1())
+    }
+
+    @Test
+    fun `part one`() {
+        assertEquals(1195150, Day06.readInput().part1())
+    }
+
+    @Test
+    fun `part two example`() {
+        assertEquals(71503, Day06.readInputExample().part2())
+    }
+
+    @Test
+    fun `part two`() {
+        assertEquals(42550411, Day06.readInput().part2())
+    }
+}
