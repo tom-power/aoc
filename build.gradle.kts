319968e976a863e45a397d0a03fc74213334719b
Tom Power <tpower21@gmail.com>
2023-12-08 15:50:14 +0000

    bump gradle, java, kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.20"
 }
 
 repositories {
@@ -19,11 +19,11 @@ dependencies {
 
 tasks {
     wrapper {
-        gradleVersion = "7.6"
+        gradleVersion = "8.5"
     }
     withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "17"
+            jvmTarget = "21"
         }
     }
     test {
