56f2a78020497aa361114039ee266da1bd0ed647
Tom Power <tp21@tuta.io>
2023-12-12 12:36:47 +0000

    day 11 23

---
+++ src/main/kotlin/aoc23/Day11.kt
@@ -0,0 +1,81 @@
+package aoc23
+
+import aoc23.Day11Domain.Observatory
+import aoc23.Day11Parser.toObservatory
+import com.github.shiguruikai.combinatoricskt.combinations
+import common.Space2D
+import common.Space2D.Parser.toPointToChars
+import common.Year23
+
+object Day11 : Year23 {
+    fun List<String>.part1(): Long =
+        toObservatory(emptySpaceMultiplier = 2)
+            .sumOfShortestPaths()
+
+    fun List<String>.part2(emptySpaceMultiplier: Int): Long =
+        toObservatory(emptySpaceMultiplier = emptySpaceMultiplier)
+            .sumOfShortestPaths()
+}
+
+object Day11Domain {
+    data class Observatory(
+        private val galaxyMap: Map<Space2D.Point, Char>,
+        private val emptySpaceMultiplier: Int
+    ) {
+        fun sumOfShortestPaths(): Long =
+            expandGalaxies()
+                .toShortestPaths()
+                .sum()
+
+        private fun expandGalaxies(): Set<Space2D.Point> =
+            galaxyMap
+                .toGalaxies()
+                .expandGalaxies(
+                    expandUpFromY = galaxyMap.emptyAlong { it.y },
+                    expandRightFromX = galaxyMap.emptyAlong { it.x },
+                    emptySpaceMultiplier = emptySpaceMultiplier,
+                )
+    }
+
+    private fun Map<Space2D.Point, Char>.emptyAlong(function: (Space2D.Point) -> Int): Set<Int> =
+        keys
+            .groupBy(function)
+            .filterValues { points -> points.all { this[it] == '.' } }
+            .keys
+
+    private fun Map<Space2D.Point, Char>.toGalaxies(): Set<Space2D.Point> =
+        this
+            .filterValues { it != '.' }
+            .keys
+
+    private fun Set<Space2D.Point>.expandGalaxies(
+        expandUpFromY: Set<Int>,
+        expandRightFromX: Set<Int>,
+        emptySpaceMultiplier: Int
+    ): Set<Space2D.Point> =
+        this.map { point ->
+            point
+                .move(
+                    direction = Space2D.Direction.Up,
+                    by = (expandUpFromY.count { it < point.y } * (emptySpaceMultiplier - 1))
+                )
+                .move(
+                    direction = Space2D.Direction.Right,
+                    by = (expandRightFromX.count { it < point.x } * (emptySpaceMultiplier - 1))
+                )
+        }.toSet()
+
+    private fun Set<Space2D.Point>.toShortestPaths(): List<Long> =
+        this.combinations(2)
+            .map { it.first().distanceTo(it.last()).toLong() }
+            .toList()
+
+}
+
+object Day11Parser {
+    fun List<String>.toObservatory(emptySpaceMultiplier: Int): Observatory =
+        Observatory(
+            galaxyMap = toPointToChars().toMap(),
+            emptySpaceMultiplier = emptySpaceMultiplier
+        )
+}
--- src/test/kotlin/aoc23/Day10Test.kt
+++ src/test/kotlin/aoc23/Day10Test.kt
@@ -6,6 +6,7 @@ import aoc23.Day10.part1
 import aoc23.Day10.part2
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class Day10Test {
@@ -19,28 +20,32 @@ class Day10Test {
         assertEquals(6714, Day10.readInput().part1())
     }
 
-    @Test
-    fun `part two example 2`() {
-        assertEquals(4, Day10.readInput("_example2").part2())
+    @Nested
+    inner class Day10TestPart2 {
+        @Test
+        fun `part two example 2`() {
+            assertEquals(4, Day10.readInput("_example2").part2())
+        }
+
+        @Test
+        fun `part two example 2a`() {
+            assertEquals(4, Day10.readInput("_example2a").part2())
+        }
+
+        @Test
+        fun `part two example 3`() {
+            assertEquals(8, Day10.readInput("_example3").part2())
+        }
+
+        @Test
+        fun `part two example 4`() {
+            assertEquals(10, Day10.readInput("_example4").part2())
+        }
+
+        @Test
+        fun `part two`() {
+            assertEquals(429, Day10.readInput().part2())
+        }
     }
 
-    @Test
-    fun `part two example 2a`() {
-        assertEquals(4, Day10.readInput("_example2a").part2())
-    }
-
-    @Test
-    fun `part two example 3`() {
-        assertEquals(8, Day10.readInput("_example3").part2())
-    }
-
-    @Test
-    fun `part two example 4`() {
-        assertEquals(10, Day10.readInput("_example4").part2())
-    }
-
-    @Test
-    fun `part two`() {
-        assertEquals(429, Day10.readInput().part2())
-    }
 }
