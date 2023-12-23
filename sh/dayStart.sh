664fcc2ea29886f776e170b41abb7c302463966a
Tom Power <tp21@tuta.io>
2023-12-23 21:43:24 +0000

    dayStart fix

--- sh/dayStart.sh
+++ sh/dayStart.sh
@@ -14,13 +14,13 @@ sed -i "s/00/$day/g" "$mainYear/Day$day.kt" &&
 sed -i "s/template/aoc$year/" "$mainYear/Day$day.kt" &&
 
 mkdir -p "$testYear" &&
-cp "$testTemplate/Day00Test.kt" "$testYear/Day$dayTest.kt" &&
-sed -i "s/Year00/Year$year/g" "$testYear/Day$dayTest.kt" &&
-sed -i "s/00/$day/g" "$testYear/Day$dayTest.kt" &&
-sed -i "s/template/aoc$year/" "$testYear/Day$dayTest.kt" &&
+cp "$testTemplate/Day00Test.kt" "$testYear/Day${day}Test.kt" &&
+sed -i "s/Year00/Year$year/g" "$testYear/Day${day}Test.kt" &&
+sed -i "s/00/$day/g" "$testYear/Day${day}Test.kt" &&
+sed -i "s/template/aoc$year/" "$testYear/Day${day}Test.kt" &&
 
 mkdir -p "$testResourcesYear"  &&
 touch "$testResourcesYear/Day$day.txt" &&
-touch "$testResourcesYear/Day$day_example.txt" &&
+touch "$testResourcesYear/Day${day}_example.txt" &&
 
 mob start  --include-uncommitted-changes --branch "day$day"
