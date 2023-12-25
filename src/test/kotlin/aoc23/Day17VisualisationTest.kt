115d5cd564c43f52f4f9d2345aa40d56586b6ecb
Tom Power <tpower21@gmail.com>
2023-12-25 08:38:36 +0000

    day 17 23 part 2

--- src/test/kotlin/aoc23/Day17VisualisationTest.kt
+++ src/test/kotlin/aoc23/Day17VisualisationTest.kt
@@ -1,6 +1,8 @@
 package aoc23
 
 import aoc23.Day17Parser.toCity
+import aoc23.Day17Solution.fourToTenStepsDirectionPredicate
+import aoc23.Day17Solution.threeStepsDirectionPredicate
 import common.Input.readInput
 import common.Input.readInputExample
 import common.Monitoring
@@ -17,7 +19,7 @@ class Day17VisualisationTest {
     inner class PartOne {
         @Test
         fun `part one example last`() {
-            framesFor(input = Day17.readInputExample())
+            framesFor(Day17.readInputExample(), threeStepsDirectionPredicate)
                 .animate()
         }
 
@@ -40,14 +42,47 @@ class Day17VisualisationTest {
             43226746555**
         """.trimIndent()
 
-            val actual = framesFor(input = Day17.readInputExample()).last().format()
+            val actual = framesFor(Day17.readInputExample(), threeStepsDirectionPredicate).last().format()
+
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Nested
+    inner class PartTwo {
+        @Test
+        fun `part two example last`() {
+            framesFor(Day17.readInputExample(), fourToTenStepsDirectionPredicate)
+                .animate()
+        }
+
+        @Test
+        fun `part one example visualisation`() {
+            val expected =
+                """
+            *********1323
+            32154535*5623
+            32552456*4254
+            34465858*5452
+            45466578*****
+            143859879845*
+            445787698776*
+            363787797965*
+            465496798688*
+            456467998645*
+            122468686556*
+            254654888773*
+            432267465553*
+        """.trimIndent()
+
+            val actual = framesFor(Day17.readInputExample(), fourToTenStepsDirectionPredicate).last().format()
 
             assertEquals(expected, actual)
         }
     }
 }
 
-private fun framesFor(input: List<String>): List<String> =
+private fun framesFor(input: List<String>, directionPredicate: DirectionPredicate): List<String> =
     Monitoring.PointCharMonitor(canvas = input.toPointToChars().toMap(), highlightWith = "*")
-        .apply { input.toCity(this).leastHeatLoss() }
+        .apply { input.toCity(this).leastHeatLoss(directionPredicate) }
         .toLoggable()
