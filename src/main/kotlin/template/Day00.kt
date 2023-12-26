2529faae6b8de724e06acffd30f66b16227ea85f
Tom Power <tp21@tuta.io>
2023-12-26 09:45:20 +0000

    tidy template

--- src/main/kotlin/template/Day00.kt
+++ src/main/kotlin/template/Day00.kt
@@ -4,29 +4,26 @@ import common.Misc.log
 import common.Year00
 import template.Day00Domain.Day00Type
 import template.Day00Parser.toDay00
-import template.Day00Solution.part1Day00
-import template.Day00Solution.part2Day00
 
 object Day00 : Year00 {
-    fun List<String>.part1(): Int = part1Day00()
-
-    fun List<String>.part2(): Int = part2Day00()
-}
-
-object Day00Solution {
-    fun List<String>.part1Day00(): Int =
-        toDay00().log()
+    fun List<String>.part1(): Int =
+        toDay00()
+            .log()
             .let { 0 }
 
-    fun List<String>.part2Day00(): Int =
-        toDay00().log()
+    fun List<String>.part2(): Int =
+        toDay00()
+            .log()
             .let { 0 }
 }
 
 object Day00Domain {
-    data class Day00Type(val value: String)
+    class Day00Type(
+        val value: String
+    )
 }
 
 object Day00Parser {
-    fun List<String>.toDay00(): List<Day00Type> = TODO()
+    fun List<String>.toDay00(): Day00Type =
+        TODO()
 }
